fc2dfc3264917a741829a998746127aa2b4266f2
Ezzeldien <ezzeldienrashad@gmail.com>
2022-06-21 13:03:19 +0200

    menu dropdown

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,7 @@ if (document.documentElement.clientWidth > 900) {
 }
 document.getElementsByTagName("main")[0].style.minHeight =
     document.documentElement.clientHeight - 80 + "px";
+
+document.getElementsByClassName("menu")[0].addEventListener("click", function () {
+    document.getElementsByClassName("dropdown")[0].classList.toggle("display-dropdown");
+})
--- users.php
+++ users.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>OurCommunity</title>
+	<title>OurCommunity users</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 	<meta name="description" content="a community for meeting friends, sending messages, chating, etc....">
@@ -10,7 +10,7 @@
 	<script type="text/javascript" src="scripts/index.js" defer></script>
 	<script src="https://kit.fontawesome.com/5cf0e9fc67.js" crossorigin="anonymous"></script>
 	<link rel="icon" href="pictures/community_logo.webp">
-	<link rel="stylesheet" href="styles/index.css" />
+	<link rel="stylesheet" href="styles/users.css" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Lobster&family=Lusitana&display=swap" rel="stylesheet">
@@ -73,6 +73,18 @@ if (isset($_POST["logout"])) {
 </header>
 <main>
 
+<h2>Other users:</h2>
+<?php
+$conn = mysqli_connect("localhost", "epiz_31976759", "xhb1FTZFr4SdTM9", "epiz_31976759_OurCommunity");
+$result = mysqli_query($conn, "SELECT * FROM Users");
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+mysqli_free_result($result);
+foreach ($users as $user) {
+	if ($user["name"] != $name) {
+		echo "<div class='user'>" . $user["name"] . "</div>";
+	}
+}
+?>
 </main>
 <footer>
 <a href="signup.php">sign up</a>
